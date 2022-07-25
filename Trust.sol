6bec59284d563dd8dacd94ebf2759947e62ee03e
bujosa <davidbujosa@gmail.com>
2022-07-25 18:43:25 -0400

    feat: add constructor and withdraw function

--- Trust.sol
+++ Trust.sol
@@ -1,5 +1,17 @@
 pragma solidity =0.8.15;
 
 contract Trust {
-    
+    address public kid;
+    uint public maturity;
+
+    constructor(address _kid, uint _maturity) payable {
+        kid = _kid;
+        maturity = block.timestamp + _maturity;
+    }
+
+    function withdraw() external {
+        require(block.timestamp >= maturity, 'too early');
+        require(msg.sender == kid, 'only kid can withdraw');
+        payable(msg.sender).transfer(address(this).balance);
+    }
 }
